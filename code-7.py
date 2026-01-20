8f59afb9d4cfc0cf20d4e811ff7b36a498c162af
A.NAGA NAVEEN CHOWDARY <113875164+naveenchowdary2324@users.noreply.github.com>
2026-01-20 12:32:36 +0530

    Add control flow statements in code-7.py

---
+++ code-7.py
@@ -0,0 +1,28 @@
+#Control Flow Statements
+from asyncio import Condition
+
+# IF Condition
+a = 100
+b = 15
+if a > b:
+    print("a is greater than b")
+
+
+# IF-Else Condition
+x = 12
+y = 32
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
+
+# elseif condition
+
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
