7f3ab3d0ef8005076709cb07a1daae6080e86b8b
A.NAGA NAVEEN CHOWDARY <113875164+naveenchowdary2324@users.noreply.github.com>
2026-01-20 12:32:14 +0530

    Add examples of logical, membership, and identity operators
    Demonstrate logical, membership, and identity operators in Python.

---
+++ code-6.py
@@ -0,0 +1,23 @@
+#Logical Operators & MemberShip Operators & Identity Operators
+
+#Logical Operators
+
+a =10
+b =20
+print("And Operator", a<b and a<10)
+print("Or Operator", a<b or a<10)
+print("NOT Operator" , not b<10)
+
+# MemberShip Operators
+
+str1 = "My name is Naveen"
+print("is" in str1)
+print("Naga" in str1)
+print("Naga" not in str1)
+
+# Identity Operators
+
+a1 =100
+b1 = 300
+print(a1 is b1)
+print(a1 is not b1)
