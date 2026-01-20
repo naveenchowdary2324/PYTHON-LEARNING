666086962f08c431e51cdd3abecf482b0a52478c
A.NAGA NAVEEN CHOWDARY <113875164+naveenchowdary2324@users.noreply.github.com>
2026-01-20 11:57:41 +0530

    Add comparison operators example in code-5.py

---
+++ code-5.py
@@ -0,0 +1,12 @@
+#Comparision Operators
+
+
+a = 100
+b = 200
+
+print("a==b",a==b) #Check if a is equal to b
+print("a!=b",a!=b) #Check if a is not equal to b
+print("a>b",a>b) #Check if a greater than b
+print("a>=b",a>=b) #Check if a greater than or equal to b
+print("a<b",a<b) #Check if a less than b
+print("a<=b",a<=b) #Check if a less than or equal to b
