e38f4d14b09073b0b0347c9c7ee32fe628efdfe2
A.NAGA NAVEEN CHOWDARY <113875164+naveenchowdary2324@users.noreply.github.com>
2026-01-20 11:48:49 +0530

    Implement arithmetic and assignment operators examples
    Added examples of arithmetic and assignment operators in Python.

---
+++ code-4.py
@@ -0,0 +1,26 @@
+# Arithmetic Operators & Assignment Operators
+
+a = 10
+b = 20
+print(a+b) #addition
+print(a-b) #Substraction
+print(a*b) #Multiplication
+print(a/b) #Division
+print(a//b) #FloorDivision
+print(a%b) #Modulus(Remainder)
+print(a**b) #Power(Exponent)
+
+# Assignment Operators
+
+x = 6
+y = 5
+x += y #x = x+y
+print(x)
+x -= y #x = x-y
+print(x)
+x *= y #x = x*y
+print(x)
+x /= y #x = x/y
+print(x)
+x **= y #x = x**y
+print(x)
